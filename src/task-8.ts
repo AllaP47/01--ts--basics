f0a12cbcb58f53cb9290a0e01cb5acd5e98b2e05
AllaP47 <allapaschinska1@gmail.com>
2026-04-22 14:59:29 +0300

    add message

--- src/task-8.ts
+++ src/task-8.ts
@@ -1,12 +1,4 @@
-function getMessage(): Promise<string> {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve("Hello from TS");
-    }, 1000);
-  });
-}
 
-getMessage().then(result => console.log(result));
 import axios from "axios";
 
 interface Post {
